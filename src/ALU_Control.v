8795e0abd9a05218b58c113206388f1e95e3f853
Gustavo Sotres <gustavonline.games@gmail.com>
2022-03-31 13:50:19 -0600

    added AND instruction support

--- src/ALU_Control.v
+++ src/ALU_Control.v
@@ -26,6 +26,7 @@ module ALU_Control
 //R types							 f7 upc  f3
 localparam R_Type_ADD		= 7'b0_000_000;
 localparam R_Type_SUB		= 7'b1_000_000;
+localparam R_Type_AND		= 7'b0_000_111;
 //I types
 localparam I_Type_ADDI		= 7'bx_001_000;
 localparam I_Type_OR			= 7'bx_001_110;
@@ -46,6 +47,7 @@ always@(selector)begin
 		//R types
 		R_Type_ADD:			alu_control_values = 4'b0000;
 		R_Type_SUB:			alu_control_values = 4'b0001;
+		R_Type_AND: 		alu_control_values = 4'b0010;
 		//I types
 		I_Type_ADDI:		alu_control_values = 4'b0000;
 		I_Type_OR:			alu_control_values = 4'b0011;
