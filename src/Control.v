0300687602132ca55e245984794d220b4159092f
Gustavo Sotres <gustavonline.games@gmail.com>
2022-03-31 23:13:22 -0600

    Added 2 more control lines
    This lines were added to handle the two previous created mux.

--- src/Control.v
+++ src/Control.v
@@ -22,32 +22,42 @@ module Control
 	output Mem_Write_o,
 	output ALU_Src_o,
 	output Reg_Write_o,
-	output [2:0]ALU_Op_o
+	output [2:0]ALU_Op_o,
+	output Save_PC_o,
+	output PC_Select_o
 );
 
 localparam R_Type				= 7'b0110011;
 localparam I_Type_LOGIC		= 7'b0010011;
 localparam I_Type_MEMORY	= 7'b0000011;
+localparam I_Type_JUMP		= 7'b1100111;
 localparam S_Type				= 7'b0100011;
 localparam U_Type				= 7'b0110111;
 localparam B_Type				= 7'b1100011;
+localparam J_Type				= 7'b1101111;
 
 
-reg [8:0] control_values;
+reg [10:0] control_values;
 
 always@(OP_i) begin	
-	case(OP_i)//                     	  876_54_3_210
-		R_Type:		 	control_values = 9'b001_00_0_000;
-		I_Type_LOGIC:	control_values = 9'b001_00_1_001;
-		I_Type_MEMORY: control_values = 9'b011_10_1_010;
-		S_Type:			control_values = 9'b000_01_1_010;
-		U_Type:			control_values = 9'b001_00_1_100;
-		B_Type:			control_values = 9'b100_00_0_101;
+	case(OP_i)//                     	  109_876_54_3_210
+		R_Type:		 	control_values = 11'b00_001_00_0_000;
+		I_Type_LOGIC:	control_values = 11'b00_001_00_1_001;
+		I_Type_MEMORY: control_values = 11'b00_011_10_1_010;
+		I_Type_JUMP: 	control_values = 11'b11_101_00_1_111;
+		S_Type:			control_values = 11'b00_000_01_1_010;
+		U_Type:			control_values = 11'b00_001_00_1_100;
+		B_Type:			control_values = 11'b00_100_00_0_101;
+		J_Type:	 		control_values = 11'b01_101_00_1_110;
 
 		default:
 			control_values= 9'b000_00_000;
 		endcase
 end	
+
+assign PC_Select_o = control_values[10];
+
+assign Save_PC_o = control_values[9];
 
 assign Branch_o = control_values[8];
 
